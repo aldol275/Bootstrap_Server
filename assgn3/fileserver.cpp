d59da449730120b585c8a15feab7bde821dfea81
solanki275 <aryansolanki094@gmail.com>
2024-09-02 19:03:13 +0530

    Second file view

--- assgn3/fileserver.cpp
+++ assgn3/fileserver.cpp
@@ -128,20 +128,10 @@ void as_client(){
 
       dataSent == -1 ? cout<<"Error in sending data : "<<endl: cout<<"message sent successfully "<<endl; 
     
-      // if(!strcmp(sendDataStr.c_str(), "bye"))
-      // {
-      //    break;
-      // }
+     
       int len;
       memset(&rcvDataBuf, 0, maxDataSize);
-      // dataRecvd = recvfrom(clientSocketFd, &rcvDataBuf, maxDataSize, flags,(struct sockaddr *)&serverSockAddressInfo,&sinSize);
-      // rcvDataStr = rcvDataBuf;
-      // cout<<"Message from server: "<<rcvDataStr.c_str()<<endl;
-      // if(!strcmp(rcvDataStr.c_str(), "bye"))
-      // {
-      //    break;
-      // }
-   //}
+     
    cout<<"AT is -"<<access_token<<endl;
    cout<<"All done closing socket now"<<endl;
    close(clientSocketFd);
@@ -353,109 +343,7 @@ void as_server(){ cout<<"Server "<<access_token<<endl;
 
 }
 /// @brief
- /*
-void server(){
-   uint16_t serverPort=3002;
-   string serverIpAddr = "127.0.0.1";
-   cout<<"Enter the ip address and port number to listen the connections for"<<endl;
-   cin>>serverIpAddr;
-   cin>>serverPort;
 
-   int serverSocketFd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-   if(!serverSocketFd)
-   {
-      cout<<"Error creating socket"<<endl;
-      exit(1);
-   }
-
-   struct sockaddr_in serverSockAddressInfo;
-   serverSockAddressInfo.sin_family = AF_INET;
-   serverSockAddressInfo.sin_port = htons(serverPort);
-   //serverSockAddressInfo.sin_addr.s_addr = INADDR_ANY;
-   //serverSockAddressInfo.sin_addr = serverIpAddr;
-
-   //cout<<"Server listening on IP:PORT"<<serverIpAddr.c_str()<<serverPort<<endl;
-   inet_pton(AF_INET, serverIpAddr.c_str(), &(serverSockAddressInfo.sin_addr));
-   memset(&(serverSockAddressInfo.sin_zero), '\0', 8);
-   printf("Server listening on IP %x:PORT %d \n",serverSockAddressInfo.sin_addr.s_addr, serverPort);
-
-
-   int ret = bind(serverSocketFd, (struct sockaddr *)&serverSockAddressInfo, sizeof(struct sockaddr)); 
-   if(ret<0)
-   {
-      cout<<"Error binding socket"<<endl;
-      close(serverSocketFd);
-      exit(1);
-   }
-
-
-   ret = listen(serverSocketFd, backLog);
-   if(!serverSocketFd)
-   {
-      cout<<"Error listening socket"<<endl;
-      close(serverSocketFd);
-      exit(1);
-   }
-
-   socklen_t sinSize = sizeof(struct sockaddr_in);
-   int flags = 0,choice=1;
-   int dataRecvd = 0, dataSent = 0;
-   struct sockaddr_in clientAddressInfo;
-   char rcvDataBuf[maxDataSize], sendDataBuf[maxDataSize];
-   char* filename;
-   string sendDataStr;
-
-   while(choice){
-
-      memset(&clientAddressInfo, 0, sizeof(struct sockaddr_in));
-      memset(&rcvDataBuf, 0, maxDataSize);
-      int newClientFd = accept(serverSocketFd, (struct sockaddr *)&clientAddressInfo, &sinSize);
-      if (!newClientFd)
-      {
-         cout<<"Error with new client connection "<<endl;
-         close(serverSocketFd);
-         exit(1);
-      }
-
-      cout<<"New client arrived"<<endl;
-      cin.ignore();
-
-      memset(&rcvDataBuf, 0, maxDataSize);
-
-      dataRecvd = recv(newClientFd, &rcvDataBuf, maxDataSize, flags);
-      cout<<rcvDataBuf<<endl;
-      if(access_token == rcvDataBuf){
-
-         if(!strcmp(rcvDataBuf, "bye"))
-         {
-            cout<<"client left"<<endl;
-            close(newClientFd);
-            break;
-         }
-         memset(&rcvDataBuf, 0, maxDataSize);
-         dataRecvd = recv(newClientFd, &rcvDataBuf, maxDataSize, flags);
-         filename = rcvDataBuf;
-         send_file_to_clients(newClientFd,filename);
-         cout<<"Data sent number of bytes"<<dataSent<<endl;
-         if(!strcmp(sendDataBuf, "bye"))
-         {
-            close(newClientFd);
-            break;
-         }
-      }
-      else{
-         cout<<"Invalid client"<<endl;
-         close(newClientFd);
-        // break;
-      }
-      cout<<"Press 1 to keep listening, else 0"<<endl;
-      cin>>choice;
-   }
-   
-
-   cout<<"All done closing server socket now"<<endl;
-   close(serverSocketFd);
-}*/
 int main()
 {
    
